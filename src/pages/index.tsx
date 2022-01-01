8634f0c730493952d724845e837062de9ca8702f
margaiwangara <margaiwangara@gmail.com>
2022-01-01 08:02:09 +0300

    refactor: added unsplash to config, modified homepage to include image component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,29 @@ import DefaultLayout from '@containers/DefaultLayout';
 import styled from 'styled-components';
 import { Button } from '@components/UI';
 import Image from 'next/image';
+import { IoHeartOutline, IoHeart, IoCheckmark, IoClose } from 'react-icons/io5';
 
 function HomePage() {
   return (
     <DefaultLayout title="Home">
       <Card>
-        <section className="card-image" />
+        <section className="card-image">
+          <Image
+            layout="fill"
+            alt="image of a cat"
+            src="https://source.unsplash.com/500x500?cats"
+          />
+          <button className="fav-button">
+            <IoHeartOutline />
+          </button>
+        </section>
         <section className="footer">
-          <NoButton>&times;</NoButton>
-          <YesButton>&#10003;</YesButton>
+          <NoButton>
+            <IoClose />
+          </NoButton>
+          <YesButton>
+            <IoCheckmark />
+          </YesButton>
         </section>
       </Card>
     </DefaultLayout>
@@ -29,14 +43,43 @@ const Card = styled.section`
   .card-image {
     width: 100%;
     flex: 1;
-    border: solid 1px ${({ theme }) => theme.colors.primary};
-    background-color: ${({ theme }) => theme.colors.white};
+    /* border: solid 1px ${({ theme }) => theme.colors.primary}; */
+    /* background-color: ${({ theme }) => theme.colors.white}; */
     border-radius: 10px;
+    position: relative;
+    overflow: hidden;
+    box-shadow: box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  }
+
+  button.fav-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background-color: ${({ theme }) => theme.colors.white};
+    border-radius: 50%;
+    height: 40px;
+    width: 40px;
+    box-shadow: 0px 0px 2.5px 0px rgba(0, 0, 0, 0.75);
+    outline: none;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.2rem;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.8;
+    }
   }
 
   .footer {
     display: flex;
     gap: 0.5rem;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    width: 100%;
   }
 `;
 
